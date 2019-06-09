f86bb576327168a021cec1782ce5f6bcf841b79f
John Clary <john.clary@austintexas.gov>
2019-06-08 22:48:15 -0700

    bumpversion for new readme

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="atd_jobutil",
-    version="0.0.1",
+    version="0.0.2",
     author="City of Austin",
     author_email="transportation.data@austintexas.gov",
     description="Utilities for registering PostgREST scripting jobs.",
